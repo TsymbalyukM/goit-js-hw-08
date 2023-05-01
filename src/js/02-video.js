98d2cad94c1452d07c88a927aef87fd99d0a6f31
TsymbalyukM <mashatsymbaliuk2@gmail.com>
2023-05-02 01:36:04 +0300

    add video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+console.log(player);
+
+player.on('timeupdate', _.throttle(saveData, 1000));
