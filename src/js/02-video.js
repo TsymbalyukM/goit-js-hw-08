1c54a8fa424fb83725ec7ba45a977e6d10548ab0
TsymbalyukM <mashatsymbaliuk2@gmail.com>
2023-05-02 09:18:42 +0300

    add 3-feedback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 console.log(player);
+const CURRENT_TIME = 'videoplayer-current-time';
 
-player.on('timeupdate', _.throttle(saveData, 1000));
+player.on('timeupdate', throttle(saveTimeData, 1000));
+
+function saveTimeData(data) {
+  localStorage.setItem(CURRENT_TIME, data.seconds);
+}
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,60 @@
+import throttle from 'lodash.throttle';
 
+const inputEl = document.querySelector('input');
+const messageEl = document.querySelector('textarea');
+const form = document.querySelector('.feedback-form');
+const FEEDBACK_FORM_STATE = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+updateForm();
+
+inputEl.addEventListener(
+  'input',
+  throttle(() => {
+    formData.email = inputEl.value;
+    localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(formData));
+  }, 500)
+);
+
+messageEl.addEventListener(
+  'input',
+  throttle(() => {
+    formData.message = messageEl.value;
+    localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(formData));
+  }, 500)
+);
+form.addEventListener('submit', handleSubmit);
+
+function updateForm() {
+  if (localStorage.getItem(FEEDBACK_FORM_STATE)) {
+    formData.email = JSON.parse(
+      localStorage.getItem(FEEDBACK_FORM_STATE)
+    ).email;
+    formData.message = JSON.parse(
+      localStorage.getItem(FEEDBACK_FORM_STATE)
+    ).message;
+    inputEl.value = formData.email;
+    messageEl.value = formData.message;
+  } else {
+    return;
+  }
+}
+function handleSubmit(event) {
+  event.preventDefault();
+
+  if (inputEl.value && messageEl.value) {
+    console.log(formData);
+    localStorage.clear();
+    event.currentTarget.reset();
+    formData = {
+      email: '',
+      message: '',
+    };
+  } else {
+    alert('Всі поля повинні бути заповнені!');
+  }
+}
